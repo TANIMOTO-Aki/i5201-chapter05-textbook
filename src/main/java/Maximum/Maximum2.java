95750ab35ce256d1266b67aed91ca97f86e7c026
TANIMOTO-Aki <kisy23011@kibireha.ac.jp>
2023-11-08 11:41:16 +0900

    応用課題5.3.1 Maximum2.javaを作成しました

---
+++ src/main/java/Maximum/Maximum2.java
@@ -0,0 +1,31 @@
+import java.io.*;
+
+public class Maximum2 {
+    public static void main(String[] args) throws IOException {
+        /* 定数と変数の宣言 */
+        final String MAX_MESSAGE = "最大値は";      //"最大値は"のメッセージ
+        String max = "";                            //最大値
+
+        /* 入力の準備 */
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+
+        /* ３つの値の入力 */
+        System.out.print("xを入力＞");
+        int x = Integer.parseInt(br.readLine());    // xを入力
+        System.out.print("yを入力＞");
+        int y = Integer.parseInt(br.readLine());    // yを入力
+        System.out.print("zを入力＞");
+        int z = Integer.parseInt(br.readLine());    // zを入力
+
+        /* 最大値の判定 */
+        int maxValue = x;
+        if (maxValue < y) {
+            maxValue = y;
+        }
+        if (maxValue < z) {
+            maxValue = z;
+        }
+        System.out.println(MAX_MESSAGE + maxValue);
+    }
+}
